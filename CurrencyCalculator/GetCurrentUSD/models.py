0fff788fb78320514fe644ce86e2613eb11bd438
nikita <midirenr@gmail.com>
2024-01-24 12:58:16 +0300

    doc: add doc-string

--- CurrencyCalculator/GetCurrentUSD/models.py
+++ CurrencyCalculator/GetCurrentUSD/models.py
@@ -4,14 +4,31 @@ from django.db import models
 
 
 class CurrencyHistoryUSD(models.Model):
+    """
+    time: время получение курса
+    value_in_rubles: стоимость доллара в рублях
+    """
     time = models.TimeField()
     value_in_rubles = models.FloatField()
 
     @classmethod
-    def set_new_value(cls, value: float) -> None:
+    def set_new_value_in_rubles(cls, value: float) -> None:
+        """
+        Установить новую стоимость доллара в рублях
+
+        :params value: стоимость USD в рублях
+        """
         current_time = datetime.now().time().replace(microsecond=0)
         cls(time=current_time, value_in_rubles=value).save()
 
     @classmethod
     def get_latest_values_in_rubles(cls):
+        """
+        Получить последние значения в рублях
+
+        Описание: возвращает queryset с последними 10 записями.
+        Записи отсортированы по убыванию времени получения (time)
+
+        :return queryset
+        """
         return cls.objects.all().order_by('-time')[:10]
--- CurrencyCalculator/GetCurrentUSD/views.py
+++ CurrencyCalculator/GetCurrentUSD/views.py
@@ -11,9 +11,14 @@ def get_currency_usd(request):
 
 
 def update_currency_usd():
+    """
+    Обновить валюту USD
+
+    Описание: функция для джобы, которая автоматически запускается каждые 10 секунд.
+    """
     response = requests.get(url="https://openexchangerates.org/api/latest.json?"
                                 "app_id=86dc21712965404094a31edb8bbd0251&"
                                 "base=USD&"
                                 "symbols=RUB")
     rub_in_usd = float(response.json()['rates']['RUB'])
-    CurrencyHistoryUSD.set_new_value(value=rub_in_usd)
+    CurrencyHistoryUSD.set_new_value_in_rubles(value=rub_in_usd)
